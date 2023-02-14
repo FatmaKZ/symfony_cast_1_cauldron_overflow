d57e49a134cff10da5287dce76e93042c87f3bd4
FatmaKZ <kaabaoui.fatma@hotmail.fr>
2023-02-14 18:41:22 +0100

    Initial commit

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class QuestionController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_homepage")
+     */
+    public function homepage(Environment $twigEnvironment)
+    {
+        // on peut faire comme ça
+        /*$html = $twigEnvironment->render('question/homepage.html.twig');
+
+        return new Response($html);*/
+
+        // ou comme ça
+        return $this->render('question/homepage.html.twig');
+    }
+
+    /**
+     * @Route("/questions/{slug}", name="app_question_show")
+     */
+    public function show($slug)
+    {
+        $answers = [
+            'Make sur out cat is sitting purrrfectly still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers
+        ]);
+    }
+}
